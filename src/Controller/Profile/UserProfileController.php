0a9c9f22e313bba2d772459eb8d5d1308ab4b307
Grozef <f.lisowski.pro@gmail.com>
2024-07-30 14:39:34 +0200

    googlebooks api htmx test

--- src/Controller/Profile/UserProfileController.php
+++ src/Controller/Profile/UserProfileController.php
@@ -2,17 +2,64 @@
 
 namespace App\Controller\Profile;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\UserReadings;
+use App\Service\ProfileService;
+use App\Service\GoogleBooksApiService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/profile', name: 'app_profile_')]
 class UserProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
+    public function __construct(
+        private readonly GoogleBooksApiService $googleBooksApiService,
+        private readonly ProfileService $profileService
+    ) {
+    }
+
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('profile/user_profile/index.html.twig', [
+        return $this->render('user_profile/index.html.twig', [
             'controller_name' => 'UserProfileController',
         ]);
     }
+
+    #[Route('/search', name: 'search')]
+    public function search(): Response
+    {
+        return $this->render('user_profile/search.html.twig');
+    }
+
+    #[Route('/search/api', name: 'search_api', methods: ['POST'])]
+    public function searchApi(Request $request): Response
+    {
+        // input --> search
+        $search = $request->request->get('search');
+
+        return $this->render('user_profile/_api.html.twig', [
+            'search' => $this->googleBooksApiService->search($search),
+        ]);
+    }
+
+    #[Route('/search/add/{id}', name: 'search_add', methods: ['GET'])]
+    public function searchAdd(string $id): Response
+    {
+        $userBook = $this->profileService->addBookToProfile($this->getUser(), $id);
+
+        return $this->redirectToRoute('app_profile_my_books', [
+            'id' => $userBook->getId(),
+        ]);
+    }
+
+    #[Route('/my-books/{id}', name: 'my_books')]
+    public function showOneBook(UserReadings $userBook): Response
+    {
+        return $this->render('user_profile/show_one_book.html.twig', [
+            'userBook' => $userBook,
+        ]);
+    }
+
 }
